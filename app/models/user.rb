c169c4537a137f08bb8621a886f689001483427b
David Peter <david@1000memories.com>
2013-03-12 20:41:37 -0400

    Enable token authentication.

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,9 +15,8 @@
 
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :token_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
   attr_accessible :email, :name, :password, :description, :entry_id, :jam_id
